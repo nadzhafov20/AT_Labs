bda854f529bde1042af2b63523b02b234288babf
Tymur Nadzhafov <115876746+nadzhafov20@users.noreply.github.com>
2023-03-20 23:50:21 +0200

    Add files via upload

---
+++ task6/src/main/java/org/example/entity/Phone.java
@@ -0,0 +1,28 @@
+package org.example.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "phone")
+@NoArgsConstructor
+public class Phone implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String number;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Phone(String number) {
+        this.number = number;
+    }
+}
+
