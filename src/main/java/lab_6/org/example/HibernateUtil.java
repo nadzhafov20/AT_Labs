6d9f1a3252a77625ea37a8b180de4f6a028081de
Tymur Nadzhafov <tamerlan.nadzhafov@gmail.com>
2023-04-29 16:41:31 +0300

    add lab6 for all labs

---
+++ src/main/java/lab_6/org/example/HibernateUtil.java
@@ -0,0 +1,50 @@
+package lab_6.org.example;
+
+import org.example.entity.Device;
+import org.example.entity.Phone;
+import org.example.entity.SocialMedia;
+import org.example.entity.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.Properties;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static SessionFactory buildSessionFactory() {
+        try {// Create properties for Hibernate configuration
+            Properties hibernateProps = new Properties();
+            hibernateProps.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+            hibernateProps.put(Environment.URL, "jdbc:mysql://localhost:3306/itstep");
+            hibernateProps.put(Environment.USER, "root");
+            hibernateProps.put(Environment.PASS, "12345");
+            hibernateProps.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            hibernateProps.put(Environment.SHOW_SQL, "true");
+            hibernateProps.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+            hibernateProps.put(Environment.HBM2DDL_AUTO, "create");
+
+
+            // Build the configuration object and register entity classes
+            Configuration configuration = new Configuration();
+            configuration.setProperties(hibernateProps);
+            configuration.addAnnotatedClass(User.class);
+            configuration.addAnnotatedClass(Phone.class);
+            configuration.addAnnotatedClass(Device.class);
+            configuration.addAnnotatedClass(SocialMedia.class);
+
+            // Build the ServiceRegistry and create the SessionFactory
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            return configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }}
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory; }
+
+}
---
+++ src/main/java/lab_6/org/example/entity/SocialMedia.java
@@ -0,0 +1,30 @@
+package lab_6.org.example.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "social_media")
+@NoArgsConstructor
+public class SocialMedia implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+
+
+    @ManyToMany(mappedBy = "socialMedia", fetch = FetchType.LAZY)
+    private List<User> users;
+
+
+    public SocialMedia(String name) {
+        this.name = name;
+    }
+}
+
